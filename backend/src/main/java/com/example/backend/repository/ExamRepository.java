d37bc8b433ea750deee6a7bdb154ed5adf7256bc
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-04-03 14:00:03 +0530

    created exam repo

---
+++ backend/src/main/java/com/example/backend/repository/ExamRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ExamRepository extends JpaRepository<Exam, Long> {
+    Optional<Exam> findByTestLink(String link);
+}
