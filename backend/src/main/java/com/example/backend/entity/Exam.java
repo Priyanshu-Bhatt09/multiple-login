db5aae2a625ef12b97714a5404fb86b5932e113b
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-04-03 13:57:35 +0530

    changed link, added getters and setters

--- backend/src/main/java/com/example/backend/entity/Exam.java
+++ backend/src/main/java/com/example/backend/entity/Exam.java
@@ -14,8 +14,32 @@ public class Exam {
     @Column(nullable = false)
     private String title;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getTestLink() {
+        return testLink;
+    }
+
+    public void setTestLink(String testLink) {
+        this.testLink = testLink;
+    }
+
     @Column(unique = true, nullable = false, updatable = false)
-    private String testLink = UUID.randomUUID().toString();
+    private String testLink;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "creator_id", nullable = false)
@@ -26,4 +50,28 @@ public class Exam {
 
     @OneToOne(mappedBy = "exam", cascade = CascadeType.ALL)
     private TestSettings settings;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public TestSettings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(TestSettings settings) {
+        this.settings = settings;
+    }
 }
