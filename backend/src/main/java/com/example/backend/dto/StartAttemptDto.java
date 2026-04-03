84b69baf767a542c424e6286c25fa9935c14ff4e
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-04-03 16:51:42 +0530

    dto for getting examId in attempt

---
+++ backend/src/main/java/com/example/backend/dto/StartAttemptDto.java
@@ -0,0 +1,13 @@
+package com.example.backend.dto;
+
+public class StartAttemptDto {
+    private Long examId;
+
+    public Long getExamId() {
+        return examId;
+    }
+
+    public void setExamId(Long examId) {
+        this.examId = examId;
+    }
+}
