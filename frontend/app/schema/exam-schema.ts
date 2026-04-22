5d824f00d74cf92bbd041fafc84c554f3256e2af
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-04-22 13:28:35 +0530

    updated schema

--- frontend/app/schema/exam-schema.ts
+++ frontend/app/schema/exam-schema.ts
@@ -2,16 +2,16 @@
 import {z} from 'zod'; //zod is a ts first schema validation library
 
 export const optionSchema = z.object({
-    text: z.string().min(1, "Options cannot be empty"), //this means the string should contain atleast one character
+    text: z.string(),
     isCorrect: z.boolean()
 });
 
 export const questionSchema = z.object({
-    text: z.string().min(1, "Question required"),
-    points: z.number().min(1),
-    negativePoint: z.number().min(0),
-    options: z.array(optionSchema).min(2),
-    paragraph: z.string().optional(),
+    text: z.string(),
+    points: z.number(),
+    negativePoint: z.number(),
+    options: z.array(optionSchema),
+    paragraph: z.string(),
     type: z.enum([
         "MCQ",
         "MCA",
@@ -20,12 +20,15 @@ export const questionSchema = z.object({
         "INPUT",
         "MATCH",
         "COMPREHENSION"
-    ])
+    ]),
+    correctOption: z.union([z.number(), z.string()]).optional().nullable(), //this field accepts a string, a number, null or nothing at all.
+    correctAnswer: z.string().optional().nullable(), //expects a string but don't throw an error if data is missing or null
+    correctCode: z.string().optional().nullable()
 });
 
 export const examSchema = z.object({
-    title: z.string().min(1),
-    questions: z.array(questionSchema).min(1)
+    title: z.string(),
+    questions: z.array(questionSchema)
 });
 
 export type ExamFormValues = z.infer<typeof examSchema>
